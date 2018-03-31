c3dc2342b3694b697ba932a57b1846b5ff31abd0
Kaushik <kaushikam@gmail.com>
2018-03-31 14:45:01 +0530

    Added stock_daily_record to tables.sql

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -16,4 +16,19 @@ CREATE TABLE stock_detail (
   CONSTRAINT pk_stock_detail FOREIGN KEY (stock_id) REFERENCES stock (id)
 );
 
+CREATE TABLE stock_daily_record (
+  record_id NUMBER(10) NOT NULL,
+  stock_id NUMBER(10) NOT NULL,
+  price_open NUMBER(10,6),
+  price_close NUMBER(10, 6),
+  price_change NUMBER(10, 6),
+  volume NUMBER(10),
+  record_date DATE,
+  CONSTRAINT pk_stock_daily_record PRIMARY KEY (record_id),
+  CONSTRAINT fk_stock_stock_daily_record FOREIGN KEY (stock_id) REFERENCES stock (id),
+  CONSTRAINT uq_record_date UNIQUE (record_date)
+);
+
 CREATE SEQUENCE stock_sequence START WITH 1 INCREMENT BY 1 CACHE 100;
+
+CREATE SEQUENCE daily_record_sequence START WITH 1 INCREMENT BY 1 CACHE 100;
